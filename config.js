98a2053e9ca3369acb4497a46a7d6807ff5b593a
qaafacademy <qaafdocs@gmail.com>
2026-09-17 22:22:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
  * Fill in the two values below (see the setup guide, steps 3 and 5). */
 window.QAAF_CONFIG = {
   // Web app URL from Apps Script → Deploy → Manage deployments (ends with /exec)
-  apiUrl: 'PASTE_WEB_APP_URL_HERE',
+  apiUrl: 'https://qaafacademy.github.io/tracker/',
 
   // OAuth Client ID from Google Cloud (ends with .apps.googleusercontent.com)
-  googleClientId: 'PASTE_CLIENT_ID_HERE'
+  googleClientId: '687957032488-j3oscp0h4l3lc6p3b6ce0uqgoh0m7cv6.apps.googleusercontent.com'
 };
 if (/^PASTE_/.test(window.QAAF_CONFIG.apiUrl)) window.QAAF_CONFIG.apiUrl = '';
 if (/^PASTE_/.test(window.QAAF_CONFIG.googleClientId)) window.QAAF_CONFIG.googleClientId = '';
